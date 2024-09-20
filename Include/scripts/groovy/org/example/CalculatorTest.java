03213006a64ae2c44236998a55c6fbacba585f73
thuyiswater <s3877746@rmit.edu.vn>
2024-09-20 22:32:14 +0700

    add test case for modulo and power

--- Include/scripts/groovy/org/example/CalculatorTest.java
+++ Include/scripts/groovy/org/example/CalculatorTest.java
@@ -366,4 +366,209 @@ public class CalculatorTest {
         assertTrue(output.contains("Support integer only!"));
         assertTrue(output.contains("Exiting the calculator. Goodbye!"));
     }
+    
+    @Test
+    public void gcdTwoPositiveNumbers() {
+        String input = "7\n27\n18\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Greatest Common Divisor (GCD) of 27 and 18 is 9"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void gcdOnePositiveOneNegativeNumber() {
+        String input = "7\n100\n-20\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Greatest Common Divisor (GCD) of 100 and -20 is 20"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void gcdTwoNegativeNumbers() {
+        String input = "7\n-72\n-27\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Greatest Common Divisor (GCD) of -72 and -27 is 9"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void gcdWithOneNumberIsZero() {
+        String input = "7\n127\n0\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Greatest Common Divisor (GCD) of 127 and 0 is 127"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void gcdTwoPrimeNumbers() {
+        String input = "7\n13\n17\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Greatest Common Divisor (GCD) of 13 and 17 is 1"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void gcdInvalidInput() {
+        // Simulating valid and invalid input
+        String input = "7\nabc\n0\nb";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();  // Method that runs gcd()
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("Support integer only!"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void lcmTwoPositiveNumbers() {
+        String input = "8\n4\n5\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Least Common Multiple (LCM) of 4 and 5 is 20"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void lcmOnePositiveOneNegativeNumber() {
+        String input = "8\n27\n-2\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Least Common Multiple (LCM) of 27 and -2 is 54"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void lcmTwoNegativeNumbers() {
+        String input = "8\n-30\n-12\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Least Common Multiple (LCM) of -30 and -12 is 60"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void lcmWithOneNumberIsZero() {
+        String input = "8\n203\n0\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Least Common Multiple (LCM) of 203 and 0 is 0"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void lcmTwoEqualNumbers() {
+        String input = "8\n123\n123\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Least Common Multiple (LCM) of 123 and 123 is 123"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void lcmTwoPrimeNumbers() {
+        String input = "8\n13\n17\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Least Common Multiple (LCM) of 13 and 17 is 221"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void lcmInvalidInput() {
+        // Simulating valid and invalid input
+        String input = "8\na\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("Support integer only!"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void fibonacciPositiveNumber() {
+        // Simulating valid and invalid input
+        String input = "9\n7\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Fibonacci of 7 is 13"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void fibonacciOfZero() {
+        // Simulating valid and invalid input
+        String input = "9\n0\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Fibonacci of 0 is 0"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void fibonacciOfOne() {
+        // Simulating valid and invalid input
+        String input = "9\n1\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("The Fibonacci of 1 is 1"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void fibonacciNegativeNumber() {
+        // Simulating valid and invalid input
+        String input = "9\n-5\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("Fibonacci is not defined for negative numbers"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
+
+    @Test
+    public void fibonacciInvalidInput() {
+        // Simulating valid and invalid input
+        String input = "9\nabc\n0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        initCalculator();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("Support integer only!"));
+        assertTrue(output.contains("Exiting the calculator. Goodbye!"));
+    }
 }
